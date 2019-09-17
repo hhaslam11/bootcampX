0665a34130b72eaba0cdb96c0ecf927edccde01b
hhaslam11 <kalebmarc@hotmail.com>
2019-09-17 15:02:15 -0700

    Add avg cohort assist time query

---
+++ 4_queries/4_avg_cohort_assist_time.sql
@@ -0,0 +1,9 @@
+SELECT cohorts.name AS name,
+       AVG(completed_at - started_at) AS avg_assist_time
+
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+
+GROUP BY cohorts.name
+ORDER BY avg_assist_time;
