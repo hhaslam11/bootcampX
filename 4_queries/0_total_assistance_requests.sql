d4e3a9fd0fb336c950bf9446f450cfac565a3143
hhaslam11 <kalebmarc@hotmail.com>
2019-09-17 14:41:23 -0700

    add total assistance requests by teacher query

---
+++ 4_queries/0_total_assistance_requests.sql
@@ -0,0 +1,6 @@
+SELECT COUNT(*) AS total_assistances,
+       teachers.name AS name
+FROM assistance_requests
+JOIN teachers ON assistance_requests.teacher_id = teachers.id
+WHERE teachers.name = 'Waylon Boehm'
+GROUP BY teachers.name;
