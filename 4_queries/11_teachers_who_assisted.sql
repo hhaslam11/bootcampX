039418e013048f47965efa5e95b1d1e0a2b8b2bb
hhaslam11 <kalebmarc@hotmail.com>
2019-09-17 18:02:19 -0700

    add teachers who assisted query

--- 4_queries/11_teachers_who_assisted.sql
+++ 4_queries/11_teachers_who_assisted.sql
@@ -1,5 +1,6 @@
 SELECT DISTINCT teachers.name AS teacher,
-                cohorts.name  AS cohort
+                cohorts.name  AS cohort,
+                COUNT(assistance_requests) AS total_assistances
 
 FROM assistance_requests
 JOIN students ON students.id = assistance_requests.student_id
@@ -7,4 +8,5 @@ JOIN cohorts  ON cohorts.id  = students.cohort_id
 JOIN teachers ON teachers.id = assistance_requests.teacher_id
 
 WHERE cohorts.name = 'JUL02'
+GROUP BY teachers.name, cohorts.name
 ORDER BY teachers.name;
